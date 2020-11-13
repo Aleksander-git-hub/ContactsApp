aec58463d71e13c753648f89d7e7c9d7aaabda0d
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2020-11-14 01:15:01 +0300

    Init8

--- contacts_app.sql
+++ contacts_app.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS contacts;
 
 CREATE TABLE users
 (
@@ -7,6 +8,16 @@ CREATE TABLE users
 	user_password varchar(100) NOT NULL,
 	user_email varchar(100) NOT NULL,
 	PRIMARY KEY(user_id)
+);
+
+CREATE TABLE contacts
+(
+    contact_id SERIAL NOT NULL,
+    contact_first_name varchar(100) NOT NULL,
+    contact_second_name varchar(100) NOT NULL,
+    contact_phone_number varchar(100) NOT NULL,
+    contact_email varchar(100) NOT NULL,
+    PRIMARY KEY(contact_id)
 );
 
 DELETE FROM users WHERE user_id > 0;
