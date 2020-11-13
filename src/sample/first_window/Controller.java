4325b6eaf8cd6c3047804694cfbe59db8b622df0
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2020-11-13 17:57:45 +0300

    Init2

--- src/sample/first_window/Controller.java
+++ src/sample/first_window/Controller.java
@@ -1,11 +1,16 @@
 package sample.first_window;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller
 {
@@ -29,8 +34,42 @@ public class Controller
 
     @FXML
     void initialize() {
+        // РЕГИСТРАЦИЯ
+        loginSignUpButton.setOnAction(event -> {
+            // закрываем firstWindow
+            loginSignUpButton.getScene().getWindow().hide();
+            // отобразим нужно окно
+            FXMLLoader loader = new FXMLLoader();
+            // указываем локацию нужного файла
+            loader.setLocation(getClass().getResource("/sample/sign_up_window/sign_up_window.fxml"));
+            // далее загружаем файл для отображения
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot(); // путь к нужному нам файлу
+            Stage stage = new Stage();
+            // указываем нужное окно, которое нам надо загрузить
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        // ВХОД
         signButton.setOnAction(event -> {
-            System.out.println("Вы нажали кнопку войти");
+            signButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/second_window/sign_in_window.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
     }
 }
